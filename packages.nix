78ad2302d6e0775fa1013adc7f1d37e5719ae833
HelpfulFish <learning_open_source_dev@proton.me>
2025-01-03 15:02:29 +1300

    feat: tmux stractpad + krita

--- packages.nix
+++ packages.nix
@@ -11,8 +11,10 @@
   home.packages = with pkgs; [
     (config.lib.nixGL.wrap anki)
     (config.lib.nixGL.wrap alacritty)
-    (config.lib.nixGL.wrap brave)
+    # (config.lib.nixGL.wrap brave)
+    brave
     fira-code
+    krita # wrap with nixGL??
     lazygit
     (config.lib.nixGL.wrap mpv)
     (config.lib.nixGL.wrap obsidian)
